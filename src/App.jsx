bdaf61cd9d1149ca1a9647e99bcfe21238d80727
jonorl <39122230+jonorl@users.noreply.github.com>
2026-03-04 15:21:03 +0300

    Added files up to updating cache revisited

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { gql } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client/react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
+import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm'
+import { useState } from 'react'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(localStorage.getItem('phonebook-user-token'))
+
+  const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
+
+  const onLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <button onClick={onLogout}>logout</button>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+}
+
+export default App
